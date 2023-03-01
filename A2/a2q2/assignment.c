7d9ac119a3f556be38c0f6d9cdacd855b011e0a5
rastin1379 <rastin1379@gmail.com>
2023-03-01 15:58:10 -0500

    Add A2Q2

--- A2/a2q2/assignment.c
+++ A2/a2q2/assignment.c
@@ -3,31 +3,42 @@
 void
 consume_enter(struct resource *resource)
 {
-    // FILL ME IN
-
+    pthread_mutex_lock(&resource->mutex);
+    while ((resource->num_consumers + 1) > resource->num_producers * resource->ratio) {
+        pthread_cond_wait(&resource->cond, &resource->mutex);
+    }
     resource->num_consumers += 1;
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 consume_exit(struct resource *resource)
 {
-    // FILL ME IN
-
+    pthread_mutex_lock(&resource->mutex);
     resource->num_consumers -= 1;
+    pthread_cond_broadcast(&resource->cond);
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_enter(struct resource *resource)
 {
-    // FILL ME IN
-
+    pthread_mutex_lock(&resource->mutex);
+    resource->num_producers += 1;
+    pthread_cond_broadcast(&resource->cond);
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_exit(struct resource *resource)
 {
-    // FILL ME IN
-
+    pthread_mutex_lock(&resource->mutex);
+    while (resource->num_consumers > (resource->num_producers - 1) * resource->ratio)
+    {
+        pthread_cond_wait(&resource->cond, &resource->mutex);
+    }
+    resource->num_producers -= 1;
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 
